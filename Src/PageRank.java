b047a99f4936a19c3e64a59a2c7b766be05a2776
Datti0812 <83442542+Datti0812@users.noreply.github.com>
2021-10-13 15:56:41 -0700

    The main driver
    Driver program to execute the mapper and driver with the given input.

---
+++ Src/PageRank.java
@@ -0,0 +1,38 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class PageRank {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: PageRank <input path> <output path>");
+            System.exit(-1);
+        }
+
+        String input = args[0];
+        String output = args[1];
+        for(int i = 0; i < 3; i++) {
+            Job job = Job.getInstance();
+            job.setJarByClass(PageRank.class);
+            job.setJobName("Page Rank" + i + "");
+
+            FileInputFormat.addInputPath(job, new Path(input));
+            FileOutputFormat.setOutputPath(job, new Path(output));
+
+            job.setMapperClass(PageRankMapper.class);
+            job.setReducerClass(PageRankReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            job.setNumReduceTasks(1);
+
+            output = output + i + "";
+            job.waitForCompletion(true);s
+            input = output + "/" + "part-r-00000";
+          }
+    context.write(NullWritable.get(), new Text(print + " " + rank_value + ""));
+  }
+}
